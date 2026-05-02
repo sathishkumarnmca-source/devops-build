c76cbf909eb0997aac58d1d65c2955e9abbdd847
sathishkumarnmca-source <sathishkumar.nmca@gmail.com>
2026-05-03 02:10:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 docker stop react-app-container || true
 docker rm react-app-container || true
-<<<<<<< HEAD
 docker run -d -p 80:80 --name reactbuild  reactuild
-=======
-docker run -d -p 80:80 --name reactbuild-app  reactbuild-app
->>>>>>> origin/dev
